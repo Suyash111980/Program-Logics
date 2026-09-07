30e27a94c0a83f13726503e42bc243bc364bc838
Suyash1116 <suyashkolekar812@gmail.com>
2026-09-07 19:16:50 +0530

    Program in python

---
+++ datatype.py
@@ -0,0 +1,33 @@
+print("Python Datatypes")
+
+x=10
+print(x,type(x))
+
+s="Suyash"
+print(s,type(s))
+
+marks=90.9
+print(marks,type(marks))
+
+li={10,20,30,40}
+print(li,type(li))
+
+price=(100,200)
+print(price,type(price))
+
+s1= { "id":10,
+      "name":"suyash"    
+    
+    }
+    
+print(s1,type(s1))
+
+
+marks=float (input("enter u r marks"))
+print(marks)
+
+cno=int(marks)
+print(cno)
+
+sno=str(cno)
+print(sno*2)
---
+++ example.py
@@ -0,0 +1,17 @@
+fn=int (input("Enter 1 number"))  
+sn=int (input("Enter 2 number")) 
+
+
+
+
+
+sqr=fn*fn
+cube=sn*sn*sn
+sum1=sqr+cube
+
+sum2=sqr+cube+sum1
+
+print(sqr)
+print(cube)
+print(sum1)
+print(sum2)
